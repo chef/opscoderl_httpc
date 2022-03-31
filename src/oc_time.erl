b6c775315221d30cebac859f3df708b02f9adebc
Lincoln Baker <lbaker@chef.io>
2022-03-31 16:06:23 -0500

    Fix compilation error (Erlang 24x upgrade).
    ===> Compiling src/oc_time.erl failed
    src/oc_time.erl:32:1: this clause for to_ms/2 cannot match because a previous clause at line 30 always matches
    src/oc_time.erl:34:1: this clause for to_ms/2 cannot match because a previous clause at line 30 always matches
    
    ===> Compiling _build/default/lib/opscoderl_httpc/src/oc_time.erl failed
    _build/default/lib/opscoderl_httpc/src/oc_time.erl:30:7: variable '_Unit' is bound multiple times in this pattern. If you mean to ignore this value, use '_' or a different underscore-prefixed name
    _build/default/lib/opscoderl_httpc/src/oc_time.erl:30:14: variable '_Unit' is bound multiple times in this pattern. If you mean to ignore this value, use '_' or a different underscore-prefixed name
    
    Signed-off-by: Lincoln Baker <lbaker@chef.io>

--- src/oc_time.erl
+++ src/oc_time.erl
@@ -27,9 +27,5 @@
 convert_units({Val, SourceUnit}, TargetUnit) ->
     Val * to_ms(SourceUnit, TargetUnit).
 
-to_ms(_Unit, _Unit) ->
-    1;
-to_ms(min, ms) ->
-    60000;
-to_ms(sec, ms) ->
-    1000.
+to_ms(_Unit1, _Unit2) ->
+    1.
